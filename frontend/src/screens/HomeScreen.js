396317cc66ff12607c2f2c2d4f8749611fb9abb1
fayaz <fayazakthar70@gmail.com>
2022-11-02 22:09:23 +0530

    sort functionality implemented

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
@@ -16,17 +16,58 @@ const HomeScreen = ({ match }) => {
 
   const dispatch = useDispatch()
 
-  const productList = useSelector((state) => state.productList)
-  const { loading, error, products, page, pages } = productList
-
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
+  const [sortBy, setSortBy] = useState('name');
+  const [data, setData] = useState([...products]);
+  console.log(data)
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber))
   }, [dispatch, keyword, pageNumber])
+  
+  useEffect(()=>{
+    const sortProducts = (sortBy)=> {
+      if (sortBy == 'name'){
+        const sorted = [...products].sort((a, b) => {
+          const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        setData(sorted);
+      }
+      else if (sortBy == 'price'){
+        const sorted = [...products].sort((a,b)=>a.price - b.price);
+        setData(sorted);
+      }  
+      else if (sortBy == 'createdAt'){
+        const sorted = [...products].sort(function(a,b){
+          return new Date(b.date) - new Date(a.date);
+          setData(sorted);
+        });
+      }
+    }
+    sortProducts(sortBy)
+  },[sortBy]);
+
+  
 
   return (
     <>
       <Meta />
       <h1>Latest Products</h1>
+      <label>Sort Products By:</label>
+      
+      <select name="name" onChange={(e) => setSortBy(e.target.value)}>
+        <option value="name">Name</option>
+        <option value="price">Price</option>
+        <option value="createdAt">Date</option>
+      </select>
       {loading ? (
         <Loader />
       ) : error ? (
@@ -34,7 +75,7 @@ const HomeScreen = ({ match }) => {
       ) : (
         <>
           <Row>
-            {products.map((product) => (
+            {data.map((product) => (
               <Col key={product._id} xs={6} sm={6} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
